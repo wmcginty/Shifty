1287133c84441488dd2dd998eff7114b01c428df
Will McGinty <mcgintw@gmail.com>
2017-12-31 12:56:59 -0600

    Fix a failing test

--- Example/Tests/SnapshotTests.swift
+++ Example/Tests/SnapshotTests.swift
@@ -22,29 +22,10 @@ class SnapshotTests: XCTestCase {
         let a = Snapshot(center: .zero, bounds: .zero, transform: CATransform3DIdentity)
         let b = Snapshot(center: CGPoint(x: 10, y: 10), bounds: .zero, transform: CATransform3DIdentity)
         let c = Snapshot(center: .zero, bounds: CGRect(origin: .zero, size: CGSize(width: 10, height: 10)), transform: CATransform3DIdentity)
-        let d = Snapshot(center: .zero, bounds: .zero, transform: CATransform3DIdentity)
-        let e = Snapshot(center: .zero, bounds: .zero, transform: CATransform3DScale(CATransform3DIdentity, 1.1, 1.1, 1.1))
+        let d = Snapshot(center: .zero, bounds: .zero, transform: CATransform3DScale(CATransform3DIdentity, 1.1, 1.1, 1.1))
         
         XCTAssertNotEqual(a, b)
         XCTAssertNotEqual(a, c)
         XCTAssertNotEqual(a, d)
-        XCTAssertNotEqual(a, e)
-    }
-    
-    func testX() {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        print("Identity")
-        print(CGAffineTransform.identity)
-        view.layer.transform = CATransform3DMakeRotation(.pi, 1, 1, 0)
-        let superview = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        superview.addSubview(view)
-        
-        print("Layer Affine")
-        print(view.layer.affineTransform())
-        
-        print("View Transform")
-        print(view.transform)
-        
-        print()
     }
 }
