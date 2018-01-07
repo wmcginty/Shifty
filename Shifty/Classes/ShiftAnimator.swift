6524d228304d52f0ef9d86c046636f444aed5554
Will McGinty <mcgintw@gmail.com>
2018-01-07 16:37:27 -0600

    Move UIViewImplicitlyAnimating conformance to separate file

--- Shifty/Classes/ShiftAnimator.swift
+++ Shifty/Classes/ShiftAnimator.swift
@@ -14,9 +14,8 @@ import Foundation
 public class ShiftAnimator: NSObject {
     
     private let shifts: [Shift]
-    
     private var destinations: [Shift: Snapshot]?
-    private var animators: [Shift: UIViewPropertyAnimator] = [:]
+    var animators: [Shift: UIViewPropertyAnimator] = [:]
 
     // MARK: Initializers
     public init(source: ShiftTransitionable, destination: ShiftTransitionable, coordinator: ShiftCoordinator = DefaultCoordinator()) {
@@ -73,55 +72,5 @@ private extension ShiftAnimator {
         animator.addCompletion { _ in shift.cleanupShiftingView(shiftingView) }
         
         return animator
-    }
-}
-
-//MARK: UIViewImplicitlyAnimating
-extension ShiftAnimator: UIViewImplicitlyAnimating {
-
-    private var representativeAnimator: UIViewPropertyAnimator? {
-        return animators.values.first
-    }
-    
-    public var state: UIViewAnimatingState {
-        return representativeAnimator?.state ?? .inactive
-    }
-    
-    public var isRunning: Bool {
-        return representativeAnimator?.isRunning ?? false
-    }
-    
-    public var isReversed: Bool {
-        get { return representativeAnimator?.isReversed ?? false }
-        set {
-            animators.values.forEach { $0.isReversed = newValue }
-        }
-    }
-    
-    public var fractionComplete: CGFloat {
-        get { return representativeAnimator?.fractionComplete ?? 0 }
-        set {
-            animators.values.forEach { $0.fractionComplete = fractionComplete }
-        }
-    }
-    
-    public func startAnimation() {
-        animators.values.forEach { $0.startAnimation() }
-    }
-    
-    public func startAnimation(afterDelay delay: TimeInterval = 0.0) {
-        animators.values.forEach { $0.startAnimation(afterDelay: delay) }
-    }
-    
-    public func pauseAnimation() {
-        animators.values.forEach { $0.pauseAnimation() }
-    }
-    
-    public func stopAnimation(_ withoutFinishing: Bool) {
-        animators.values.forEach { $0.stopAnimation(withoutFinishing) }
-    }
-    
-    public func finishAnimation(at finalPosition: UIViewAnimatingPosition) {
-        animators.values.forEach { $0.finishAnimation(at: finalPosition) }
     }
 }
