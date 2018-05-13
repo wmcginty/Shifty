e2e941fba4d1602dc121b5dd3e24bd03613ecd79
Will McGinty <mcgintw@gmail.com>
2018-05-13 16:13:26 -0500

    A few more project tweaks

---
+++ Examples/Shifty-iOSExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Shifty-iOSExample
+//
+//  Created by William McGinty on 5/13/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Sources/Supporting Files/Shifty.h
@@ -0,0 +1,19 @@
+//
+//  Shifty.h
+//  Shifty
+//
+//  Created by William McGinty on 5/13/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shifty.
+FOUNDATION_EXPORT double Shifty_VersionNumber;
+
+//! Project version string for Shifty.
+FOUNDATION_EXPORT const unsigned char Shifty_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shifty_/PublicHeader.h>
+
+
